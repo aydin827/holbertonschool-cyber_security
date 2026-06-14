fa1470b7b619560ddc2b5f154968e82a2aca6fa7
aydin827 <13597@holbertonstudents.com>
2026-06-14 11:48:04 -0400

    D

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-subfinder -d $1 -silent | while read sub; do echo "$sub,$(dig +short $sub)"; done | tee $1.txt
+#!/bin/bash
+subfinder -d $1 -silent | while read sub; do echo "$sub,$(dig +short $sub)" ; done > $1.txt
