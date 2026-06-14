537d4e8f8702682e4b92bfb5581f816d91235412
aydin827 <13597@holbertonstudents.com>
2026-06-14 12:13:40 -0400

    fix

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-subfinder -d $1 -silent | while read sub; do echo "$sub,$(dig +short $sub)"; done > $1.txt
+subfinder -d $1 -silent > subs.txt
+while read sub; do
+  ip=$(dig +short $sub)
+  echo "$sub,$ip"
+done < subs.txt > $1.txt
