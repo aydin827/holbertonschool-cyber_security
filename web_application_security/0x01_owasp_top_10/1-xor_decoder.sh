ec95e655e99571a9ba5e17684b3025299d01b8fd
aydin827 <13597@holbertonstudents.com>
2026-07-02 09:22:11 -0400

    G

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -3,4 +3,4 @@
 input="$1"
 clean="${input#\{xor\}}"
 
-echo "$clean" | base64 -d
+echo "$clean" | base64 -d | xxd -p | tr -d '\n' | sed 's/../& /g' | awk '{for(i=1;i<=NF;i++) printf "%c", strtonum("0x"$i)^42}'
