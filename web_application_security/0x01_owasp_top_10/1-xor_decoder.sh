1f23e72734f089377d87d9c93f8748d54dcc3825
aydin827 <13597@holbertonstudents.com>
2026-07-02 09:27:34 -0400

    G

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,33 +1,14 @@
 #!/bin/bash
-# Description: Decodes a WebSphere XOR encoded password hash.
 
-# Strip the {xor} prefix if present
-input_hash=${1#"{xor}"}
+input="$1"
+clean="${input#\{xor\}}"
 
-# Decode from Base64 and process byte by byte
-decoded_bytes=$(echo -n "$input_hash" | base64 -d 2>/dev/null)
+echo "$clean" | base64 -d | python3 -c '
+import sys
 
-# Exit if base64 decoding fails or input is empty
-if [ -z "$decoded_bytes" ]
-then
-	exit 1
-fi
+data = sys.stdin.buffer.read()
+key = b"websphere"
 
-# Loop through each character to perform XOR with '_'
-for (( i=0; i<${#decoded_bytes}; i++ ))
-do
-	# Get the character
-	char="${decoded_bytes:$i:1}"
-	
-	# Get the ASCII decimal value of the character
-	printf -v ascii_val '%d' "'$char"
-	
-	# Perform XOR with 95 (ASCII for '_')
-	xor_val=$(( ascii_val ^ 95 ))
-	
-	# Print the decoded character
-	printf "\\x$(printf '%02x' "$xor_val")"
-done
-
-# Print a final newline character as required
-printf "\n"
+out = bytes([data[i] ^ key[i % len(key)] for i in range(len(data))])
+print(out.decode(errors="ignore"))
+'
