9ba06de8a4992acbd2cdf8b8815d70bb253fc7bd
aydin827 <13597@holbertonstudents.com>
2026-08-16 17:33:19 +0400

    F

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-grep -i "accepted" auth.log | awk '{for(i=1;i<=NF;i++) if($i=="from") print $(i+1)}' | sort -u | wc -l
+
+grep -E "Failed password|Invalid user" auth.log | awk -F 'from ' '{print $2}' | awk '{print $1}' | sort -u > /tmp/failed_ips
+grep "Accepted password" auth.log | awk -F 'from ' '{print $2}' | awk '{print $1}' | sort -u > /tmp/accepted_ips
+comm -12 /tmp/failed_ips /tmp/accepted_ips | wc -l
